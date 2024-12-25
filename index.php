a8e8ae7b7a5e6d198ed53ae56aeb225a367880aa
ASHU JUNIOR <juniorashu36@gmail.com>
2024-12-25 04:45:09 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>first php code</title>
+</head>
+<body>
+
+    
+
+ <?php  
+ $friend = array("kevin","keren","salaza","pado","fresg","poki","junior");
+ echo $friend[2];
+ ?>
+    
+
+      </form>
+</body>
+</html>
